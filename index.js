99e053afdfeeb802ce5645390669b6cb392022d3
wing-ho <genrong.he@gmail.com>
2018-10-14 23:53:33 +0800

    修复移动目录dom结构导致的目录丢失问题

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function adapter(str){
     decodeEntities: false
   });
   //修改org-mode生成的table-of-contents的dom以适配maupassant主题
-  let toc = $("#table-of-contents").attr("id","toc").addClass("toc-article");
+  let toc = $("#table-of-contents").attr("id","toc").addClass("toc-article").wrap('<div class="clear"></div>');
   let title = $("#toc").children("h2")
   title.replaceWith($('<div class="toc-title">'+title.html()+'</div>'));
 
@@ -16,8 +16,7 @@ function adapter(str){
     var sub = parent.children("li");
     sub.addClass("toc-item toc-level-"+level).children("a").addClass("toc-link toc-text");
     arguments.callee(sub.children("ul,ol").addClass("toc-child"),level+1);
-  })($("#text-table-of-contents",toc).children("ul,ol").addClass("toc"),2);
-  toc.insertBefore(".post-content").wrap('<div class="clear"></div>')
+  })($("#text-table-of-contents").children("ul,ol").addClass("toc"),2);
   //修改图片或表格的标题的class
   $(".figure").attr("class","caption");
   return $.html();
